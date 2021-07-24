2ee42cff519f194447b6edfedbe22ec3920f705d
김유현 <sustkadl12@gmail.com>
2021-07-24 11:32:09 +0000

    2021-07-24 update

--- shopping_mall/order/models.py
+++ shopping_mall/order/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Order(models.Model):
     user = models.ForeignKey('user.User', on_delete=models.CASCADE, verbose_name='사용자')
     product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name='상품')
-    register_date = models.DateField(auto_now_add=True, verbose_name='등록날짜')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
     
     def __str__(self):
         return str(self.user) + ' ' + str(self.product)

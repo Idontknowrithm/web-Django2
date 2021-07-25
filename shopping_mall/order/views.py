e23503dbfcb7397a3d5d9b2598481ea904dddbd6
김유현 <sustkadl12@gmail.com>
2021-07-25 08:18:21 +0000

    2021-07-25 update

--- shopping_mall/order/views.py
+++ shopping_mall/order/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import FormView
+from .forms import RegisterForm
 
 # Create your views here.
+
+class OrderCreate(FormView):
+    # form view를 화면을 보여주는 용도로 사용하지 않으니 template_name은 필요없음
+    # template_name = 'register_product.html'
+    form_class = RegisterForm
+    success_url = '/product/'
+    
+    # form 전달이 실패했을 때 같은 페이지로 리다이렉트
+    def form_invalid(self, form):
+        return redirect('/product/' + str(form.product))
+    
+    # form을 전달할 때 어떤 인자값을 보낼 것인 지 결정
+    def get_form_kwargs(self, **kwargs):
+        # kw에 기존 인자값을 저장
+        kw = super().get_form_kwargs(**kwargs)
+        # kw에 request 인자값을 추가해서 저장
+        kw.update({
+            'request': self.request
+        })
+        return kw
--- shopping_mall/product/views.py
+++ shopping_mall/product/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
+from order.forms import RegisterForm as OrderForm
 
 # Create your views here.
 
@@ -14,3 +15,15 @@ class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm
     success_url = '/product/'
+class ProductDetail(DetailView):
+    template_name = 'product_detail.html'
+    queryset = Product.objects.all()
+    context_object_name = 'product'
+    
+    # 원하는 데이터를 넣을 수 있는 함수 제공
+    def get_context_data(self, **kwargs):
+        # detailview가 자동으로 전달해주는 데이터를 먼저 만들어주고 나서
+        context = super().get_context_data(**kwargs)
+        # 내가 원하는 데이터를 추가
+        context['form'] = OrderForm(self.request)
+        return context
